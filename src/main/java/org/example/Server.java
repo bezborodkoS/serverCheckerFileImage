51ecd5996f3ab73ad53d44d4f9e6c406bc0c0dbb
circleofdeath <deltaalphaengi@proton.me>
2024-11-13 20:16:04 +0200

    Level 2 done

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,11 +1,10 @@
 package org.example;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Server {
     private static final int PORT = 8081;
@@ -24,6 +23,7 @@ public class Server {
                     // Прийом файлу
                     String fileName = dataInputStream.readUTF();
                     int fileSize = dataInputStream.readInt();
+                    String content = dataInputStream.readUTF();
 
                     if (fileSize <= 1024) {
                         byte[] buffer = new byte[(int) fileSize];
@@ -36,12 +36,15 @@ public class Server {
 
                         System.out.println("Файл сохранен: " + fileName);
                         //отправка обратно
-                        dataOutputStream.writeUTF("Файл ополучен и отправлен");
+                        //it's sends metadata too, but it's not necessary because you have this file
+                        dataOutputStream.writeUTF("1");
                         dataOutputStream.writeLong(fileSize);
                         dataOutputStream.write(buffer);
+
+                        Files.writeString(new File(fileName).toPath(), content);
                     } else {
                         System.out.println("файл слишком большой");
-                        dataOutputStream.writeUTF("файл больше 1кб ");
+                        dataOutputStream.writeUTF("2");
                     }
                 } catch (IOException e) {
                     System.out.println("Клиент " + e.getMessage());
